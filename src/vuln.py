7d88c55645115a6696f234ee5856c3e9c713372c
Andy D <8628078+rpigu-i@users.noreply.github.com>
2024-06-05 13:47:32 -0400

    Apply code scanning fix for flask app is run in debug mode
    Co-authored-by: GitHub Advanced Security, powered by CodeQL and AI <62310815+github-advanced-security[bot]@users.noreply.github.com>

--- src/vuln.py
+++ src/vuln.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask
 app = Flask(__name__)
 
@@ -6,4 +7,4 @@ def hello_world():
     return 'Hello, World!'
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=os.getenv('ENV') != 'production')
